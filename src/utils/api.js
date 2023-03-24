2739ddc5157d2b28b75780f1e56d769bc98df6f9
Deepdarshan <deepdarshan21@gmail.com>
2023-03-24 12:26:02 +0530

    fix: update url after deplyment
    Signed-off-by: Deepdarshan <deepdarshan21@gmail.com>

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,19 +1,16 @@
 import axios from "axios";
 
-const serverApi = "http://localhost:5002/api";
-// const serverApi = "https://sheltered-plateau-62204.herokuapp.com/api";
+// const serverApi = "http://localhost:5002/api";
+const serverApi = "https://online-classroom-meet-client-production.up.railway.app/api";
 
 export const getRoomExists = async (roomId) => {
-
     const response = await axios.get(`${serverApi}/room-exists/${roomId}`);
 
     return response.data;
 };
 
 export const getTURNCredentials = async () => {
-
     const response = await axios.get(`${serverApi}/get-turn-credentials`);
 
     return response.data;
 };
-
